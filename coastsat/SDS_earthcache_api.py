a914dd4eddeac287da7248da3561586eb566a2f2
walter zesk <walter@conformlab.com>
2024-04-04 11:01:53 -0400

    earthcache working for med. res and I organized older example files.

--- coastsat/SDS_earthcache_api.py
+++ coastsat/SDS_earthcache_api.py
@@ -13,21 +13,21 @@ from coastsat import SDS_earthcache_client
 
 # always call this function first before using anything else
 # does an initial setup
-def initialize(api_key):
+def initialize_client(api_key,max_cost=0):
   repo = 'CoastSat'
   root_path = os.getcwd()[ 0 : os.getcwd().find( repo ) + len ( repo )]
   cfg_path = os.path.join( root_path, 'earthcache-cfg' )
   global client
-  client = SDS_earthcache_client.EcClient(cfg_path, api_key)
+  client = SDS_earthcache_client.EcClient(cfg_path, api_key,max_cost)
+  return client
 
 # use this function to directly create a pipeline
 # the 'image_type_id' parameter refers to what type of
 # images you want for the output
 # these are the available id's: https://support.skywatch.com/hc/en-us/articles/7297565063067-Available-Outputs
-def retrieve_images_earthcache(api_key, name, aoi, start_date, end_date, image_type_id, interval='30d', resolution='low', **kwargs):
+def retrieve_images_earthcache(client, name, aoi, start_date, end_date, image_type_id, interval='30d', resolution='low', **kwargs):
   pipeline_name = name
   
-  global client
   status, result = client.createPipeline(    
                                           name=pipeline_name,
                                           start_date=start_date,
@@ -51,8 +51,7 @@ def retrieve_images_earthcache(api_key, name, aoi, start_date, end_date, image_t
   
 # use this function to check the status of a given pipeline
 # 200 is a good result!  
-def checkStatus(pipeline_name):
-  global client
+def checkStatus(client, pipeline_name):
   pipeline_id = client.getPipelineIdFromName(pipeline_name)
   status, result = client.getIntervalResults( pipeline_id )
   return status, result
@@ -61,8 +60,7 @@ def checkStatus(pipeline_name):
 # call this function to get the images once the pipeline is ready
 # make sure to pass in the name of the pipeline 
 # returns a list of the images   
-def download_images(pipeline_name):
-  global client
+def download_images(client, pipeline_name):
   id = client.getPipelineIdFromName(pipeline_name)
   
   status, result = client.getIntervalResults(id)
@@ -113,8 +111,8 @@ def search(aoi, window, resolution, **kwargs):
 # status, result = SDS_earthcache_api.create_pipeline_from_search(___, ___)
 # https://api-docs.earthcache.com/#tag/pipelines/operation/PipelineCreate
 
-  def create_pipeline_from_search(search_id, search_results):
-    global client
+  def create_pipeline_from_search(client, search_id, search_results):
+    
     status, result = client.createPipelineFromSearch(search_id, search_results)
     return status, result
   
@@ -122,7 +120,7 @@ def search(aoi, window, resolution, **kwargs):
 # Calculate cost of area and intervals of a pipeline, 
 # and the probability of collection of any tasking intervals
 # https://api-docs.earthcache.com/#tag/pipelinePost 
-  def calculatePrice(self, resolution, location, start_date, end_date):
-    global client
+  def calculatePrice(client, resolution, location, start_date, end_date):
+  
     status, result = client.calculatePrice(resolution, location, start_date, end_date)
     return status, result
